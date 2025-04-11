d6488994de1119eca72826119a435ae32d3df5df
Dennis Liu <genius_dennis1224@hotmail.com>
2025-04-11 18:18:28 -0400

    Feature: Add form validation to registration page

--- Registration_App/Models/RegisterViewModel.cs
+++ Registration_App/Models/RegisterViewModel.cs
@@ -4,22 +4,23 @@ namespace Registration_App.Models
 {
     public class RegisterViewModel
     {
-
+        [Required(ErrorMessage = "First Name is required")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
-
+        [Required(ErrorMessage = "Last Name is required")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
-
+        [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-
+        [Required(ErrorMessage = "Confirm Password is required")]
         [Compare("Password", ErrorMessage = "Passwords do not match")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
